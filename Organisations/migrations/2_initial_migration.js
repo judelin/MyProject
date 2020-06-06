19bde8d912c2b0074ebbef646bf3df7ae440c613
Judelin <seide_judelin@yahoo.fr>
2020-06-06 03:53:04 -0400

    contrat Organisations

--- Organisations/migrations/2_initial_migration.js
+++ Organisations/migrations/2_initial_migration.js
@@ -2,6 +2,6 @@ const Organisation = artifacts.require("Organisation");
 
 
 module.exports = function(deployer) {
-  deployer.deploy(Organisation);
+  deployer.deploy(Organisation,{gas: 4612388, from: "0xAa901cc7a4e94c3ee668e014c1Faa36358434224"});
 };
 //deployer.deploy(Organisation,{gas: 4612388, from: "0xAa901cc7a4e94c3ee668e014c1Faa36358434224"})
