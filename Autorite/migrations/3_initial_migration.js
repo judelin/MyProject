9a5d3118cddbae56661e1b660e72fe27be239fd7
Judelin <seide_judelin@yahoo.fr>
2020-06-06 03:53:46 -0400

    contrat Autorite

--- Autorite/migrations/3_initial_migration.js
+++ Autorite/migrations/3_initial_migration.js
@@ -1,5 +1,7 @@
 const Autorite = artifacts.require("Autorite");
 
 module.exports = function(deployer) {
-  deployer.deploy(Autorite);
+	 deployer.deploy(Autorite,{gas: 4612388, from: "0x1a4Ba1BE15D5983e43B6A070c9489ce452CD4ae1"});
+  //deployer.deploy(Autorite);
 };
+//0x1a4Ba1BE15D5983e43B6A070c9489ce452CD4ae1
